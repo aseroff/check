b8853f08d293dc17c554b0353d28ce78289e69fc
Andrew Seroff <aseroff17@gmail.com>
2017-07-11 10:39:46 -0700

    setup

---
+++ app/assets/javascripts/components/games.jsx
@@ -0,0 +1,7 @@
+var Games = React.createClass({
+	render: function() {
+		return (
+			<h1>Check</h1>
+		)
+	}
+});
---
+++ db/migrate/20170711172449_create_games.rb
@@ -0,0 +1,12 @@
+class CreateGames < ActiveRecord::Migration[5.1]
+  def change
+    create_table :games do |t|
+      t.string :title
+      t.text :description
+      t.integer :year
+      t.string :img_url
+
+      t.timestamps
+    end
+  end
+end
