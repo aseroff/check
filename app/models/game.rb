8e3fa00b6d45a5b3f0a7ac16296a88d84c63b3b3
Andrew Seroff <aseroff17@gmail.com>
2017-07-11 18:16:24 -0700

    into heroku

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,2 +1,19 @@
+require 'httparty'
+
 class Game < ApplicationRecord
+	def self.import(id)
+		game = Game.find_by(id: id)
+		if game
+
+		else
+			resp = HTTParty.get('https://www.boardgamegeek.com/xmlapi2/thing?id=' + id.to_s)
+			resp = Nokogiri::XML.parse(resp.body)
+			game = Game.create(title: (resp.xpath('//name').first[:value]),
+								description: (resp.xpath('//description').first.children.first.to_s),
+								year: (resp.xpath('//yearpublished').first.attributes.first.last.value),
+								img_url: (resp.xpath('//image').first.children.last.to_s),
+								id: id)
+		end
+		game
+	end
 end
