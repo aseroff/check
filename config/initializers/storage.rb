434780b4a207143d1d5576a90fed61792d9feb27
Andrew Seroff <aseroff17@gmail.com>
2017-07-17 16:12:41 -0700

    change to bucket

--- config/initializers/storage.rb
+++ config/initializers/storage.rb
@@ -4,8 +4,8 @@ CarrierWave.configure do |config|
       provider:              'AWS',
       aws_access_key_id:     ENV["aws_access_id"],
       aws_secret_access_key: ENV["aws_access_secret_key"],
-      region: 'us-east-1'
+      region: 'us-west-1'
   }
-  config.fog_directory  = "check.data"
+  config.fog_directory  = "checkdata"
   config.fog_public     = false
 end
