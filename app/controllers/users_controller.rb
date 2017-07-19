cb078f044a6c8bb359b47371e5543f3784b15c1e
Andrew Seroff <aseroff17@gmail.com>
2017-07-19 10:16:49 -0700

    display followers/following

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,20 +1,31 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :update, :destroy]
+  before_action :set_user, :count_follows
 
   def show
-    @following = @user.following
-    @followers = @user.followers
+  end
+
+  def following
+    @users = User.where("id IN (?)", @user.following)
+  end
+
+  def followers
+    @users = User.where("id IN (?)", @user.followers)
   end
 
     private
     # Use callbacks to share common setup or constraints between actions.
     def set_user
-      @user = User.friendly.find(params[:id])
+      @user = User.friendly.find(params[:id] || params[:user_id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
       params.require(:user)
+    end
+
+    def count_follows
+      @following = @user.following.size.to_s
+      @followers = @user.followers.size.to_s
     end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   resources :posts, path: 'checkin'
   resources :relations
   devise_for :users
-  resources :users
+  resources :users do 
+    get "following"
+    get "followers"
+  end
   resources :games
   root 'posts#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
