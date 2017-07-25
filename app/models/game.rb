e8ff4d7f7f402b69be4c06a677aa8ec7b85a3e00
andy <aseroff17@gmail.com>
2017-07-25 13:20:26 -0700

    fix typo, this should do it

--- app/models/game.rb
+++ app/models/game.rb
@@ -17,7 +17,7 @@ class Game < ApplicationRecord
 				game = Game.new(id: id, title: resp.xpath('//name').first[:value])
 				game[:description] = (resp.xpath('//description').first.children.first.to_s.gsub('&amp;#10;&amp;#10;', ' ')) if resp.xpath('//description').first
 				game[:year] = (resp.xpath('//yearpublished').first.attributes.first.last.value) if resp.xpath('//yearpublished').first
-				game[:image_url] = (MiniMagick::Image.open(resp.xpath('//image').first.children.last.to_s)) if resp.xpath('//image').first
+				game[:img_url] = (MiniMagick::Image.open(resp.xpath('//image').first.children.last.to_s)) if resp.xpath('//image').first
 				game.save
 			end
 		else
