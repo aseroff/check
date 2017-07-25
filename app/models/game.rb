61d984cef0348de122b1c85c15cc604357ec784c
andy <aseroff17@gmail.com>
2017-07-25 15:25:55 -0700

    dumbfix

--- app/models/game.rb
+++ app/models/game.rb
@@ -19,7 +19,7 @@ class Game < ApplicationRecord
 
 	def self.import(id)
 		game = Game.find_by(id: id)
-		if game.nil?
+		if game.nil? || game[:img_url][-4..-1] != '.jpg'
 			resp = HTTParty.get('https://www.boardgamegeek.com/xmlapi2/thing?id=' + id.to_s + '&type=boardgame')
 			resp = Nokogiri::XML.parse(resp.body)
 			if resp.xpath('//name').first
@@ -31,7 +31,6 @@ class Game < ApplicationRecord
 			end
 		else
 			game.update_attribute(:description, game.description.html_safe.gsub('&amp;#10;', ' ')) 
-			game.img_url = (MiniMagick::Image.open(resp.xpath('//image').first.children.last.to_s)) if game[:img_url][-4..-1] != '.jpg'
 		end
 		game
 	end
