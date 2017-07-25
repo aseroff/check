16c4303304d678758a411eedb5797fa0d953859e
andy <aseroff17@gmail.com>
2017-07-25 12:59:53 -0700

    maybe game importing

--- app/models/game.rb
+++ app/models/game.rb
@@ -13,13 +13,15 @@ class Game < ApplicationRecord
 		unless game
 			resp = HTTParty.get('https://www.boardgamegeek.com/xmlapi2/thing?id=' + id.to_s)
 			resp = Nokogiri::XML.parse(resp.body)
-			game = Game.create(title: (resp.xpath('//name').first[:value]),
-								description: (resp.xpath('//description').first.children.first.to_s),
-								year: (resp.xpath('//yearpublished').first.attributes.first.last.value),
-								img_url: MiniMagick::Image.open(resp.xpath('//image').first.children.last.to_s),
+			game = Game.new(title: (resp.xpath('//name').first[:value]),
+								description: (resp.xpath('//description').first.children.first.to_s.gsub('&amp;#10;&amp;#10;', ' ') if resp.xpath('//description').first),
+								year: (resp.xpath('//yearpublished').first.attributes.first.last.value if resp.xpath('//yearpublished').first),
+								img_url: (MiniMagick::Image.open(resp.xpath('//image').first.children.last.to_s if resp.xpath('//image').first),
 								id: id)
+		else
+			game.update_attribute(:description, game.description.gsub('&amp;#10;&amp;#10;', ' ')) 
 		end
-		game
+		game.save
 	end
 
 	def self.search(term)
