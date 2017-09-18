e9c9b34b28c21195bdda6972f5e83b54f826020e
andy <aseroff17@gmail.com>
2017-09-18 15:39:56 -0700

    just a tweak to env names

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -1,4 +1,5 @@
 Rails.application.configure do
+  config.app_name = "Tokens (staging)"
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
