02911647a88d828d531d98efc120d0d558bf84a9
andy <aseroff17@gmail.com>
2017-09-25 11:01:44 -0700

    422 treated like a regular error

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -3,6 +3,10 @@ class ErrorsController < ApplicationController
     render(:status => 404)
   end
 
+  def rejected
+  	render(:status => 422)
+  end
+
   def internal_server_error
     render(:status => 500)
   end
--- config/routes.rb
+++ config/routes.rb
@@ -22,6 +22,7 @@ Rails.application.routes.draw do
   get 'errors/not_found'
   get 'errors/internal_server_error'
   match "/404", :to => "errors#not_found", :via => :all
+  match "/422", :to => "errors#rejected", :via => :all
   match "/500", :to => "errors#internal_server_error", :via => :all
   root 'posts#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
