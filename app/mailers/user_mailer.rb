ba6e75ed1b61a417cec0e26e53933a6e1f3b2d0e
andy <aseroff17@gmail.com>
2017-09-26 15:35:26 -0700

    play w displayed email sender

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,6 +2,7 @@ class UserMailer < Devise::Mailer
   helper :application
   include Devise::Controllers::UrlHelpers
   default template_path: 'devise/mailer'
+  default from: ('Tokens <no-reply@' + Rails.application.config.app_domain + '>' )
   layout 'mailer'
 
 end
