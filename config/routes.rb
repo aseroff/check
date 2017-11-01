581e42043aa72bba8f9cb3ced542e866fab71883
Andrew Seroff <aseroff17@gmail.com>
2017-11-01 16:15:36 -0700

    api branch

--- config/routes.rb
+++ config/routes.rb
@@ -26,4 +26,12 @@ Rails.application.routes.draw do
   match "/422", :to => "errors#rejected", :via => :all
   match "/500", :to => "errors#internal_server_error", :via => :all
   root 'posts#index'
+
+  namespace :api do
+    post   'login'   => 'sessions#create'
+    delete 'logout'  => 'sessions#destroy'
+    get    'verify'  => 'sessions#verify_access_token'
+    resources :users, param: :access_token
+    resources :password_resets, only: [:new, :create, :edit, :update]
+  end
 end
