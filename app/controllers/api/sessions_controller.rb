8a2dfeb6595b81201c2a844c01991ccf53b6987e
Andrew Seroff <aseroff17@gmail.com>
2017-11-02 12:32:26 -0700

    back to text maybe

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -8,18 +8,18 @@ module Api
         if user && user.valid_password?(params[:session][:password])
           access_token = SecureRandom.urlsafe_base64
           user.update(access_token: access_token)
-          render json: user.access_token, status: 200
+          render text: user.access_token, status: 200
         else
-          render json: "Email and password combination are invalid", status: 422
+          render text: "Email and password combination are invalid", status: 422
         end
     end
 
     #Verifies the access_token so the client app would know if to login the user.
     def verify_access_token
         if User.find_by(email: params[:session][:email].downcase).access_token == params[:session][:access_token]
-          render json: "verified", status: 200
+          render text: "verified", status: 200
         else
-          render json: "Token failed verification", status: 422
+          render text: "Token failed verification", status: 422
         end
     end
 
