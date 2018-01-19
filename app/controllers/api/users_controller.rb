cddf4c947fc0ab19dec2e9f6d1c1d6e812bad1da
Andrew Seroff <aseroff17@gmail.com>
2018-01-19 14:44:54 -0800

    fix9 to users

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -5,6 +5,11 @@ module Api
     def show
       @user = User.friendly.find(params[:id] || params[:user_id])
       @posts = @user.posts.order(created_at: :desc).paginate(page: params[:page], per_page: 10)
+      @following = @user.following.size.to_s
+      @followers = @user.followers.size.to_s
+      @posts_count = @user.posts.size.to_s
+      @favorited_count = @user.favorites.size.to_s
+      @owned_count = @user.owned.size.to_s
       respond_to do |format|
         format.json { render :template => "api/users/user.json" }
       end
--- app/views/api/users/user.json.jbuilder
+++ app/views/api/users/user.json.jbuilder
@@ -1,2 +1,8 @@
 json.extract! @user, :id, :username, :avatar
 json.url user_url(@user, format: :json)
+json.following_count @following
+json.followers_count @followers
+json.posts_count @posts_count
+json.favorited_count @favorited_count
+json.owned_count @owned_count
+json.posts @posts, partial: "posts/post", as: :post
