0174d34564b934c92325aa06c41a98dc0807e5e1
Andrew Seroff <aseroff17@gmail.com>
2018-01-19 13:56:12 -0800

    fix4

--- app/controllers/api/games_controller.rb
+++ app/controllers/api/games_controller.rb
@@ -1,8 +1,8 @@
 module Api
   class GamesController < ApplicationController
   before_action :set_game, only: [:show, :edit, :update, :destroy]
-  before_action :find_relations, only: [:show]
   before_action :count_stats, only: [:show]
+  before_action :find_relations, only: [:show]
 
   # GET /games
   # GET /games.json
@@ -54,8 +54,21 @@ module Api
       @game = Game.friendly.find(params[:id])
     end
 
+    def count_stats
+      @posts_count = @game.posts.size
+      @favorites_count = @game.favorites.size
+      @owned_count = @game.owned.size
+    end
+
     def look_up_authenticated_user
       @current_user = User.find_by(access_token: params[:access_token])
+    end
+    
+    def find_relations
+      if current_user
+        @favorite_relation = Relation.find_by(user_id: current_user.id, related_id: @game.id, relationship:"favorite")
+        @owned_relation = Relation.find_by(user_id: current_user.id, related_id: @game.id, relationship:"owns")
+      end
     end
 
 end
